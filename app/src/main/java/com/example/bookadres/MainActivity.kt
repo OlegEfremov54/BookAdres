b7ddd173369d0b23cbf20ce81a9f6ce9f1f77e2f
OlegEfremov54 <ole_f@mail.ru>
2024-11-08 23:11:55 +0300

    Initial commit

---
+++ app/src/main/java/com/example/bookadres/MainActivity.kt
@@ -0,0 +1,113 @@
+package com.example.bookadres
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ListView
+import android.widget.Toast
+import androidx.appcompat.widget.Toolbar
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var toolbarMain: Toolbar
+
+    private lateinit var nameET:EditText
+    private lateinit var familyET:EditText
+    private lateinit var adresET:EditText
+    private lateinit var phoneET:EditText
+
+    private lateinit var listUsersLV: ListView
+
+    private lateinit var saveBTN:Button
+
+    val users: MutableList<Person> = mutableListOf()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        toolbarMain = findViewById(R.id.toolbarMain)
+        setSupportActionBar(toolbarMain)
+        title = " Адресная книга"
+        toolbarMain.subtitle = "  Версия 1. Стартовая страница"
+        toolbarMain.setLogo(R.drawable.adresbook)
+
+        nameET = findViewById(R.id.nameET)
+        familyET = findViewById(R.id.familyET)
+        adresET = findViewById(R.id.adresET)
+        phoneET = findViewById(R.id.phoneET)
+        saveBTN = findViewById(R.id.saveBTN)
+
+        listUsersLV = findViewById(R.id.listUsersLV)
+
+
+        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, users)
+        listUsersLV.adapter = adapter
+
+        saveBTN.setOnClickListener {
+            if (nameET.text.isEmpty() ||
+                familyET.text.isEmpty() ||
+                adresET.text.isEmpty() ||
+                phoneET.text.isEmpty()
+            ) {
+                Toast.makeText(this, "Заполните все поля", Toast.LENGTH_LONG).show()
+                return@setOnClickListener
+            }
+
+            users.add(Person(nameET.text.toString(),
+                familyET.text.toString(),
+                adresET.text.toString(),
+                phoneET.text.toString()))
+
+            adapter.notifyDataSetChanged()
+                nameET.text.clear()
+                familyET.text.clear()
+                adresET.text.clear()
+                phoneET.text.clear()
+
+        }
+
+        listUsersLV.onItemClickListener =
+            AdapterView.OnItemClickListener { parent, view, position, id ->
+                val user = adapter.getItem(position)
+                val intent = Intent(this, MainActivity2::class.java)
+                intent.putExtra(Person::class.java.simpleName, user)
+                startActivity(intent)
+
+            }
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main,menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.infoMenuMain -> {
+                Toast.makeText(applicationContext, "Автор Ефремов О.В. Создан 9.11.2024",
+                    Toast.LENGTH_LONG).show()
+            }
+            R.id.exitMenuMain ->{
+                Toast.makeText(applicationContext, "Работа приложения завершена",
+                    Toast.LENGTH_LONG).show()
+                finish()
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+}
---
+++ app/src/main/java/com/example/bookadres/MainActivity2.kt
@@ -0,0 +1,93 @@
+package com.example.bookadres
+
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity2 : AppCompatActivity() {
+    private lateinit var toolbarSec: Toolbar
+
+    private lateinit var nameTV: TextView
+    private lateinit var familyTV: TextView
+    private lateinit var adresTV: TextView
+    private lateinit var phoneTV: TextView
+
+    private lateinit var buttonBackBTN: Button
+
+    var user: Person? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main2)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        toolbarSec = findViewById(R.id.toolbarSec)
+        setSupportActionBar(toolbarSec)
+        title = " Адресная книга"
+        toolbarSec.subtitle = "Подробная информация профиля"
+        toolbarSec.setLogo(R.drawable.adresbook2)
+
+        nameTV = findViewById(R.id.nameTV)
+        familyTV = findViewById(R.id.familyTV)
+        adresTV = findViewById(R.id.adresTV)
+        phoneTV = findViewById(R.id.phoneTV)
+        buttonBackBTN = findViewById(R.id.buttonBackBTN)
+
+        user = intent.extras?.getParcelable(Person::class.java.simpleName) as Person?
+        val name = user?.name.toString()
+        val family = user?.family.toString()
+        val adres = user?.adres.toString()
+        val phone = user?.phone.toString()
+
+        nameTV.text = name
+        familyTV.text = family
+        adresTV.text = adres
+        phoneTV.text = phone
+
+        buttonBackBTN.setOnClickListener { view ->
+            finish()
+        }
+    }
+
+
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main,menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.infoMenuMain -> {
+                Toast.makeText(
+                    applicationContext, "Автор Ефремов О.В. Создан 9.11.2024",
+                    Toast.LENGTH_LONG
+                ).show()
+            }
+
+            R.id.exitMenuMain -> {
+                Toast.makeText(
+                    applicationContext, "Работа приложения завершена",
+                    Toast.LENGTH_LONG
+                ).show()
+                finishAffinity()
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+}
+
+
+
